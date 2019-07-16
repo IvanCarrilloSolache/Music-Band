312c624a0fcc4a19e0150a3bf217278a647e61cc
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-15 21:13:02 -0500

    styling MiddleDrawer.js: implemented drawer content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/Navbar';
-
+import MiddleDrawer from './components/Drawer/MiddleDrawer';
 
 function App() {
   return (
-    <div>
+    <div style={{height: '100%'}}>
       <NavBar />
+      <MiddleDrawer />
       <main style={{marginTop: '5vw'}}>
         <p>Content goes here!</p>
       </main>
---
+++ src/components/Drawer/MiddleDrawer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const middleDrawer = props => (
+    <nav className="middle-Drawer">
+        <ul>
+            <li><a href="/">BAND</a></li>
+            <li><a href="/">MUSIC</a></li>
+            <li><a href="/">TOUR</a></li>
+            <li><a href="/">GALLERY</a></li>
+            <li><a href="/">CONTACT</a></li>
+        </ul>
+    </nav>
+)
+
+export default middleDrawer;
