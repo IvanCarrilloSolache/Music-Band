de47040525e236967ecdd5a3e95bec1dbc4ca1b1
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-19 17:20:10 -0500

    styling MainHeadline component: content centered

--- src/components/MainHeadline.js
+++ src/components/MainHeadline.js
@@ -3,17 +3,15 @@ import "./MainHeadline.css"
 
 const mainHeadline = props => {
     
-    return (
-        <div  className="main-headline">
-        <header>
+    return (  
+        <header className="headline-content">
             <p>nuestro álbum más reciente</p>
             <h1>FORTALEZA</h1>
+            <div>
+                <button>Echale un vistazo</button>
+                <button>Miranos en vivo</button>
+            </div>
         </header>
-        <div>
-            <button>Echale un vistazo</button>
-            <button>Miranos en vivo</button>
-        </div>
-        </div>
     );
 };
 
