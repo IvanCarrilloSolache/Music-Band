997c650150f6900c4d1ef9f2ffcc8a929530d883
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-19 14:08:50 -0500

    MainHeadline component: added content/buttons

--- src/components/MainHeadline.js
+++ src/components/MainHeadline.js
@@ -4,8 +4,15 @@ import "./MainHeadline.css"
 const mainHeadline = props => {
     
     return (
-        <div className="main-headline">
-            Main Headline
+        <div  className="main-headline">
+        <header>
+            <p>nuestro álbum más reciente</p>
+            <h1>FORTALEZA</h1>
+        </header>
+        <div>
+            <button>Echale un vistazo</button>
+            <button>Miranos en vivo</button>
+        </div>
         </div>
     );
 };
