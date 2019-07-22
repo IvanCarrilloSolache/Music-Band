4e4cc42bc0ef057eea6328254c6d20dc85af9207
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-22 14:56:55 -0500

    implementing react-router: haddled edgecases for non existent path/home component showing in every path

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Band from "./containers/Band";
 import Music from "./containers/Music";
 import Tour from "./containers/Tour";
 import Gallery from "./containers/Gallery";
 import Contact from "./containers/Contact";
+import Error from "./containers/Error";
+
 
 const Routes = () => (
     <BrowserRouter>
-        <div>
-            <Route path="/" component={Home} />
+        <Switch>
+            <Route path="/" component={Home} exact />
             <Route path="/Band" component={Band} />
             <Route path="/Music" component={Music} />
             <Route path="/Tour" component={Tour} />
             <Route path="/Gallery" component={Gallery} />
             <Route path="/Contact" component={Contact} />
-        </div>
+            <Route component={Error} />
+        </Switch>
     </BrowserRouter>
 );
     
-    export default Routes;
+export default Routes;
