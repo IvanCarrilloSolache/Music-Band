5a7d4954ad784450bb9313b1e33cfd253e7e2d28
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-23 11:06:20 -0500

    styling band component: content centered/text aligned/font styled

--- src/components/containers/Band.js
+++ src/components/containers/Band.js
@@ -3,36 +3,38 @@ import './Band.css'
 
 const Band = () => (
 <div className="band-content">
-    <h1 style={{ backgroundColor: '#75a9f9' }}>HOW IT ALL STARTED</h1>
-    <p style={{ backgroundColor: 'white' }}>
-        Donec id elit non mi porta gravida at eget metus. 
-        Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. 
-        Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
-        Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
-        ut fermentum massa justo sit amet risus.
-    </p>
-    <p style={{ backgroundColor: 'white' }}>
-        Sed posuere consectetur est at lobortis. Aenean eu leo quam. 
-        Pellentesque ornare sem lacinia quam venenatis vestibulum. 
-        Cras mattis consectetur purus sit amet fermentum. Etiam porta sem malesuada magna mollis euismod.
-        Cras mattis consectetur purus sit amet fermentum. Donec ullamcorper nulla non metus auctor fringilla. 
-        Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, 
-        vel scelerisque nisl consectetur et. Etiam porta sem malesuada magna mollis euismod.
-    </p>
-    <p style={{ backgroundColor: 'white' }}>
-        Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna. 
-        Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. 
-        Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus.
-    </p>
-    <p style={{ backgroundColor: 'white' }}>
-        Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur. 
-        Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. 
-        Donec sed odio dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, 
-        dapibus ac facilisis in, egestas eget quam. Etiam porta sem malesuada magna mollis euismod. 
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, 
-        egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-        Vestibulum id ligula porta felis euismod semper.
-    </p>
+    <h1>HOW IT ALL STARTED</h1>
+    <div className="band-p">
+        <p>
+            Donec id elit non mi porta gravida at eget metus. 
+            Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. 
+            Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
+            Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
+            ut fermentum massa justo sit amet risus.
+        </p>
+        <p>
+            Sed posuere consectetur est at lobortis. Aenean eu leo quam. 
+            Pellentesque ornare sem lacinia quam venenatis vestibulum. 
+            Cras mattis consectetur purus sit amet fermentum. Etiam porta sem malesuada magna mollis euismod.
+            Cras mattis consectetur purus sit amet fermentum. Donec ullamcorper nulla non metus auctor fringilla. 
+            Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, 
+            vel scelerisque nisl consectetur et. Etiam porta sem malesuada magna mollis euismod.
+        </p>
+        <p>
+            Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna. 
+            Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. 
+            Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus.
+        </p>
+        <p>
+            Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur. 
+            Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. 
+            Donec sed odio dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, 
+            dapibus ac facilisis in, egestas eget quam. Etiam porta sem malesuada magna mollis euismod. 
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, 
+            egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+            Vestibulum id ligula porta felis euismod semper.
+        </p>
+    </div>
 </div>
 );
 
