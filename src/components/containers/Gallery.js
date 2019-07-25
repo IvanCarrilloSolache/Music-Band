9dbfd6b4495143fcf3f9140819f5fc073679b255
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-25 12:24:22 -0500

    styling Gallery component: carousel centered/resized/applied responsiveness

--- src/components/containers/Gallery.js
+++ src/components/containers/Gallery.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Gallery, GallryImage, GalleryImage } from 'react-gesture-gallery';
+import { Gallery, GalleryImage } from 'react-gesture-gallery';
 import { images } from "./images";
+import "./Gallery.css";
 
 const GalleryComp = props => {
     const [index, setIndex] = React.useState(0)
     return ( 
-        <Gallery index={index} onRequestChange={i => { setIndex(i);}}>
-            {images.map(image => (<GalleryImage src={image} />))}
+        <div  className="carousel">
+        <Gallery
+            index={index} 
+            onRequestChange={i => { 
+                setIndex(i)
+            }}
+        >
+            {images.map(image => (
+                <GalleryImage className="image-oriented" src={image} />
+            ))}
         </Gallery>
+        </div>
     )
 }
-
 
 export default GalleryComp;
