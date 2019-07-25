13557d990aa618388d486f3b6ef599419a6000e7
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-24 23:27:57 -0500

    adding content to Tour component: implemented style sheet

--- src/components/containers/Tour.js
+++ src/components/containers/Tour.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import './Tour.css'
 const Tour = () => (
-<div>
-    <p style={{ backgroundColor: '#75a9f9' }}>Tour</p>
+<div style={{ backgroundColor: '#75a9f9' }}>
+    <h1>we're coming to your city</h1>
+    <div className="tour-date-row">
+        <p>Jul 24, 2019</p>
+        <p>Borgata Hotel Casino & Spa- Music</p>
+        <p>Atlanta City, NJ</p>
+        <button>Buy Tickets</button>
+    </div>
 </div>
 );
 
