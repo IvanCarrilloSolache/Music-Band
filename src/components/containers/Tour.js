465fb74151662519b2db2e1c033d0e6b734d3d21
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-25 00:03:58 -0500

    adding content to Tour component: implemented grid layout for tour dates

--- src/components/containers/Tour.js
+++ src/components/containers/Tour.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './Tour.css'
+
 const Tour = () => (
-<div style={{ backgroundColor: '#75a9f9' }}>
-    <h1>we're coming to your city</h1>
-    <div className="tour-date-row">
-        <p>Jul 24, 2019</p>
-        <p>Borgata Hotel Casino & Spa- Music</p>
-        <p>Atlanta City, NJ</p>
-        <button>Buy Tickets</button>
+    <div style={{ backgroundColor: '#75a9f9' }}>
+        <h1>we're coming to your city</h1>
+        <div className="tour-date-grid">
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+            <div className="grid-item">Jul 24, 2019</div>
+            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
+            <div className="grid-item">Atlanta City, NJ</div>  
+            <div className="grid-item"><button>Buy Tickets</button></div>
+        </div>
     </div>
-</div>
 );
 
 export default Tour;
