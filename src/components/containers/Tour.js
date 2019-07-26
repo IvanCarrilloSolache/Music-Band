c10ada2470526c6a8f489fec95d786c24d43566d
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-26 00:23:53 -0500

    styling Tour component: removed visual aids/resized-styled buttom/resized font

--- src/components/containers/Tour.js
+++ src/components/containers/Tour.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Tour.css'
 
 const Tour = () => (
-    <div style={{ backgroundColor: '#75a9f9' }}>
+    <div>
         <h1>we're coming to your city</h1>
         <div className="tour-date-grid">
             <div className="grid-item">Jul 24, 2019</div>
@@ -33,10 +33,7 @@ const Tour = () => (
             <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
             <div className="grid-item">Atlanta City, NJ</div>  
             <div className="grid-item"><button>Buy Tickets</button></div>
-            <div className="grid-item">Jul 24, 2019</div>
-            <div className="grid-item">Borgata Hotel Casino & Spa- Music</div>
-            <div className="grid-item">Atlanta City, NJ</div>  
-            <div className="grid-item"><button>Buy Tickets</button></div>
+            
         </div>
     </div>
 );
