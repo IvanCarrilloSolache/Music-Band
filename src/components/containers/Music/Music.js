ff90fc6aacb6af1ec11f0fc7d10bca6d0e32c7f2
home <Home@softbank220001002216.bbtec.net>
2019-08-08 16:55:58 -0500

    adding content to music component: feature track content added

--- src/components/containers/Music/Music.js
+++ src/components/containers/Music/Music.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import AudioPlayer from './AudioPlayer'
+import './Music.css'
+import album from '../../../assets/album.jpg';
 
 const Music = () => (
 <div>
     <h1>MUSIC BY METHOD</h1>
     <div style={{backgroundColor: 'red'}}>
-        <div className="feauture-track" style={{backgroundColor: 'green'}}>
-            < AudioPlayer />
+        <div className="music-section" style={{backgroundColor: 'green'}}>
+            
+            <div className="feauture-track">
+                <div className="album-img">
+                    <img src={album} alt="album image" />
+                </div>
+                <div className="feature-album">
+                    <p>Featured album</p>
+                    <h3>ALBUM TITLE</h3>
+                    <p>album details</p>
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
